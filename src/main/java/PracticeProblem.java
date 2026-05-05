12a8932780e19223536b9815f2bf5ebbb891f96d
A.Set <343303426@tdsb.ca>
2026-05-05 11:19:40 -0400

    gytr

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,4 +4,35 @@ public class PracticeProblem {
 
 	}
 
+	//q1
+	public static int getAge (String[] names, int[] ages, String name) {
+		
+		int count = 0;
+		int age = 0; 
+
+		for (int i = 0; i < names.length; i++) {
+			if (!(names[i].matches(name))) {
+				count++;
+			}
+			else {
+				ages[count] = age;
+				return age;
+			}
+		}
+		ages[count] = age;
+		return age;
+
+	}
+
+	//q2
+	/*public static int[] createLengthArray (String[] arr) {
+	
+	} */
+
+	//q3
+	/* public static swapPeople (String[] names, int[] ages, String name, String name2) {
+	
+	}*/
+
 }
+
